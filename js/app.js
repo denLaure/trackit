cbfa35d9e3408f1c33c1fe18f216d1edc06ab1fb
Denis Laure <den.a.laure@gmail.com>
2015-11-22 20:31:51 +0100

    Add more day cards

--- js/app.js
+++ js/app.js
@@ -5,15 +5,21 @@ app.config(function($mdIconProvider) {
 });
 
 $(window).load(function() {
-   $("md-whiteframe").click(function() {
-      if($("md-whiteframe").attr("opened") === "true") {
-         $("md-whiteframe").children(".card-content").hide();
-         $("md-whiteframe").children(".card-header").children(".close-card-icon").hide();
-         $("md-whiteframe").attr("opened", "false");
+   $(".day-card").each(function( index ) {
+     if($(this).attr("opened") === "false") {
+         $(this).children(".card-content").hide();
+         $(this).children(".card-header").children(".close-card-icon").hide();
+      }
+   });
+   $(".day-card").click(function() {
+      if($(this).attr("opened") === "true") {
+         $(this).children(".card-content").hide();
+         $(this).children(".card-header").children(".close-card-icon").hide();
+         $(this).attr("opened", "false");
       } else {
-         $("md-whiteframe").children(".card-content").show();
-         $("md-whiteframe").attr("opened", "true");
-         $("md-whiteframe").children(".card-header").children(".close-card-icon").show();
+         $(this).children(".card-content").show();
+         $(this).attr("opened", "true");
+         $(this).children(".card-header").children(".close-card-icon").show();
       }
    });
 });
