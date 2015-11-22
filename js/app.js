e79b0bf58873ade0fb7e82dee130707ca9da7ec9
Denis Laure <den.a.laure@gmail.com>
2015-11-22 20:38:20 +0100

    Show arrow icon when day card is closed

--- js/app.js
+++ js/app.js
@@ -6,20 +6,21 @@ app.config(function($mdIconProvider) {
 
 $(window).load(function() {
    $(".day-card").each(function( index ) {
-     if($(this).attr("opened") === "false") {
+      if($(this).attr("opened") === "false") {
          $(this).children(".card-content").hide();
+      } else {
          $(this).children(".card-header").children(".close-card-icon").hide();
       }
    });
    $(".day-card").click(function() {
       if($(this).attr("opened") === "true") {
          $(this).children(".card-content").hide();
-         $(this).children(".card-header").children(".close-card-icon").hide();
+         $(this).children(".card-header").children(".close-card-icon").show();
          $(this).attr("opened", "false");
       } else {
          $(this).children(".card-content").show();
          $(this).attr("opened", "true");
-         $(this).children(".card-header").children(".close-card-icon").show();
+         $(this).children(".card-header").children(".close-card-icon").hide();
       }
    });
 });
